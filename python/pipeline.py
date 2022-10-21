2f7f9868a46809d6c1758f047601fc0faf76cfab
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-10-21 15:01:49 +0200

    add exceptions

--- python/pipeline.py
+++ python/pipeline.py
@@ -1,3 +1,11 @@
+class ExceptionTestsFailed (Exception):
+    pass
+
+
+class ExceptionDeploymentFailed(Exception):
+    pass
+
+
 class Pipeline:
     def __init__(self, config, emailer, log):
         self.config = config
@@ -5,13 +13,18 @@ class Pipeline:
         self.log = log
 
     def run(self, project):
-        if not project.has_tests():
-            self.log.info("No tests")
-            self.deploy_project(project)
-        else:
-            self.run_tests(project)
-            if project.is_run_tests_success():
+        try:
+            if not project.has_tests():
+                self.log.info("No tests")
                 self.deploy_project(project)
+            else:
+                self.run_tests(project)
+                if project.is_run_tests_success():
+                    self.deploy_project(project)
+        except ExceptionTestsFailed:
+            self.send_email_summary("Tests failed")
+        except ExceptionDeploymentFailed:
+            self.send_email_summary("Deployment failed")
 
     def send_email_summary(self, email_summary):
         if self.config.send_email_summary():
@@ -21,16 +34,16 @@ class Pipeline:
             self.log.info("Email disabled")
 
     def deploy_project(self, project):
-        if project.is_deploy_success():
-            self.log.info("Deployment successful")
-            self.send_email_summary("Deployment completed successfully")
-        else:
+        if not project.is_deploy_success():
             self.log.error("Deployment failed")
-            self.send_email_summary("Deployment failed")
+            raise ExceptionDeploymentFailed()
+
+        self.log.info("Deployment successful")
+        self.send_email_summary("Deployment completed successfully")
 
     def run_tests(self, project):
-        if project.is_run_tests_success():
-            self.log.info("Tests passed")
-        else:
+        if not project.is_run_tests_success():
             self.log.error("Tests failed")
-            self.send_email_summary("Tests failed")
+            raise ExceptionTestsFailed
+
+        self.log.info("Tests passed")
