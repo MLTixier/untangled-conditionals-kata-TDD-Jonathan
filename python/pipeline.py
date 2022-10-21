1b0d07b202a5da67115a1ace248fe24d0fd44df8
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-10-21 15:46:39 +0200

    separate function to create summary and function to send email

--- python/pipeline.py
+++ python/pipeline.py
@@ -10,20 +10,25 @@ class Pipeline:
 
         tests_passed = self.run_tests(project, tests_passed)
         deploy_successful = self.deploy_project(deploy_successful, project, tests_passed)
-        self.send_email_summary(deploy_successful, tests_passed)
+        summary = self.define_email_summary(deploy_successful, tests_passed)
+        self.send_email_summary(summary)
 
-    def send_email_summary(self, deploy_successful, tests_passed):
+    def send_email_summary(self, summary):
         if self.config.send_email_summary():
             self.log.info("Sending email")
-            if tests_passed:
-                if deploy_successful:
-                    self.emailer.send("Deployment completed successfully")
-                else:
-                    self.emailer.send("Deployment failed")
-            else:
-                self.emailer.send("Tests failed")
+            self.emailer.send(summary)
         else:
             self.log.info("Email disabled")
+
+    def define_email_summary(self, deploy_successful, tests_passed):
+        if tests_passed:
+            if deploy_successful:
+                summary = "Deployment completed successfully"
+            else:
+                summary = "Deployment failed"
+        else:
+            summary = "Tests failed"
+        return summary
 
     def deploy_project(self, deploy_successful, project, tests_passed):
         if tests_passed:
