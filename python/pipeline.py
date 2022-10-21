701002df51859b819fe8d0f8ebb232d03aa27ad2
Marie-Laure TIXIER <marielaure.tixier@gmail.com>
2022-10-21 16:06:44 +0200

    variable tests_passed deleted from method send_email_summary()

--- python/pipeline.py
+++ python/pipeline.py
@@ -1,3 +1,7 @@
+class ExceptionTestsFailed(Exception):
+    pass
+
+
 class Pipeline:
     def __init__(self, config, emailer, log):
         self.config = config
@@ -5,16 +9,13 @@ class Pipeline:
         self.log = log
 
     def run(self, project):
-        tests_passed = False
-        deploy_successful = False
-
-        tests_passed = self.run_tests(project)
-        if not tests_passed:
-            deploy_successful = False
-        else:
+        try:
+            self.run_tests(project)
             deploy_successful = self.deploy_project(project)
-        summary = self.define_email_summary(deploy_successful, tests_passed)
-        self.send_email_summary(summary)
+            summary = self.define_email_summary(deploy_successful)
+            self.send_email_summary(summary)
+        except ExceptionTestsFailed:
+            self.send_email_summary("Tests failed")
 
     def send_email_summary(self, summary):
         if self.config.send_email_summary():
@@ -23,14 +24,11 @@ class Pipeline:
         else:
             self.log.info("Email disabled")
 
-    def define_email_summary(self, deploy_successful, tests_passed):
-        if tests_passed:
-            if deploy_successful:
-                summary = "Deployment completed successfully"
-            else:
-                summary = "Deployment failed"
+    def define_email_summary(self, deploy_successful):
+        if deploy_successful:
+            summary = "Deployment completed successfully"
         else:
-            summary = "Tests failed"
+            summary = "Deployment failed"
         return summary
 
     def deploy_project(self, project):
@@ -44,11 +42,11 @@ class Pipeline:
     def run_tests(self, project):
         if not project.has_tests():
             self.log.info("No tests")
-            return True
+            return
 
         if "success" != project.run_tests():
             self.log.error("Tests failed")
-            return False
+            raise ExceptionTestsFailed
 
         self.log.info("Tests passed")
-        return True
+
